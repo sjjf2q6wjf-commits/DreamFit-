88854c2898659c046ceec90e519334fb43c9fbe1
sjjf2q6wjf-commits <sjjf2q6wjf@privaterelay.appleid.com>
2026-06-22 03:02:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
+let members = JSON.parse(localStorage.getItem("members")) || [];
+
+function displayMembers() {
+
+let table = document.getElementById("memberTable");
+
+table.innerHTML = "";
+
+members.forEach(member => {
+
+table.innerHTML += `
+<tr>
+<td>${member.name}</td>
+<td>${member.phone}</td>
+<td>${member.plan}</td>
+</tr>
+`;
+
+});
+
+}
+
 function addMember() {
 
 let name = document.getElementById("memberName").value;
 let phone = document.getElementById("memberPhone").value;
 let plan = document.getElementById("memberPlan").value;
 
-let table = document.getElementById("memberTable");
+members.push({
+name:name,
+phone:phone,
+plan:plan
+});
 
-table.innerHTML += `
-<tr>
-<td>${name}</td>
-<td>${phone}</td>
-<td>${plan}</td>
-</tr>
-`;
+localStorage.setItem("members", JSON.stringify(members));
+
+displayMembers();
 
 }
+
+displayMembers();
