ea0cd058e9b2206330a207421be57e57ca206627
sjjf2q6wjf-commits <sjjf2q6wjf@privaterelay.appleid.com>
2026-06-22 02:44:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-js/script.js
+function addMember() {
+
+let name = document.getElementById("memberName").value;
+let phone = document.getElementById("memberPhone").value;
+let plan = document.getElementById("memberPlan").value;
+
+let table = document.getElementById("memberTable");
+
+table.innerHTML += `
+<tr>
+<td>${name}</td>
+<td>${phone}</td>
+<td>${plan}</td>
+</tr>
+`;
+
+}
