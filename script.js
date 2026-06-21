365338bd400f458b6b2ab367f0c20cefe323253a
sjjf2q6wjf-commits <sjjf2q6wjf@privaterelay.appleid.com>
2026-06-22 03:05:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,21 @@ let members = JSON.parse(localStorage.getItem("members")) || [];
 function displayMembers() {
 
 let table = document.getElementById("memberTable");
-
 table.innerHTML = "";
 
-members.forEach(member => {
+members.forEach((member, index) => {
 
 table.innerHTML += `
 <tr>
+<td>${member.id}</td>
 <td>${member.name}</td>
 <td>${member.phone}</td>
 <td>${member.plan}</td>
+<td>
+<button onclick="deleteMember(${index})">
+Delete
+</button>
+</td>
 </tr>
 `;
 
@@ -26,11 +31,24 @@ let name = document.getElementById("memberName").value;
 let phone = document.getElementById("memberPhone").value;
 let plan = document.getElementById("memberPlan").value;
 
+let id = "DF" + String(members.length + 1).padStart(3, "0");
+
 members.push({
-name:name,
-phone:phone,
-plan:plan
+id: id,
+name: name,
+phone: phone,
+plan: plan
 });
+
+localStorage.setItem("members", JSON.stringify(members));
+
+displayMembers();
+
+}
+
+function deleteMember(index) {
+
+members.splice(index, 1);
 
 localStorage.setItem("members", JSON.stringify(members));
 
